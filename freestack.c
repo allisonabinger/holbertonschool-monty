a40467a8f5636cd8d55e790440e1b72985bb23d3
Allison <allisonabinger@gmail.com>
2023-08-14 16:55:58 -0500

    made freestack.c

---
+++ freestack.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ * freeStack - frees stack
+ * @head: head node of list
+ */
+void freestack(stack_t *head)
+{
+	stack_t *current;
+	stack_t *tmp;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		tmp = current->next;
+		free(current);
+		current = tmp;
+
+	}
+}
